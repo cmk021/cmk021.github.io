f5c1916b4be5aa4f6c57d1eb54fcf2db22342278
cmk021 <60639772+cmk021@users.noreply.github.com>
2020-02-14 00:33:11 +0800

    Add files via upload

---
+++ demo0212/gen_workspace.js
@@ -0,0 +1,44 @@
+var workspace;
+
+//每關可使用的最大方塊數量
+let maxblock = [5,5,5,5,5,5,5,5,5,5,
+               5,6,10,10,10,10,10,10,14,10];
+
+function gen_workspace(toolbox_id){
+		//var toolbox_id = "toolbox0?";
+
+		var toolbox = document.getElementById(toolbox_id);
+	   	var options = {
+	 
+		  	toolbox : toolbox,
+		   
+		  	collapse : true,
+		  	comments : true,
+		  	disable : true,
+		  	maxBlocks : maxblock[ Number( toolbox_id.substr(-2) ) ],                 //Infinity,
+		  	trashcan : true,
+		  	horizontalLayout : false,
+		  	toolboxPosition : 'start',
+		  	css : true,
+		  	media : 'media/',    //
+		  	rtl : false,
+		  	scrollbars : true,
+		  	sounds : true,
+		  	oneBasedIndex : true,
+		  	zoom : {
+		  		controls : true,
+		  		wheel : true,
+		  		startScale : 1,
+		  		maxScale : 3,
+		  		minScale : 0.3,
+		  		scaleSpeed : 1.2
+		    }
+
+		};
+
+        let b1 = document.getElementById('blocklyDiv');
+        b1.innerHTML="";
+	  	workspace = Blockly.inject('blocklyDiv',options );
+	      //{toolbox: document.getElementById('toolbox')});
+        //console.log(b1.innerHTML);
+}
