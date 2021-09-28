0aa551cb73da14ce143ffc4a7aee6ad8d016efe2
Chen-Ming-Gu <60639772+cmk021@users.noreply.github.com>
2021-09-29 05:06:55 +0800

    Add files via upload

---
+++ C2 test/c2Rockman/offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1632863114,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/tiledbackground.png",
+		"images/player-sheet0.png",
+		"images/player-sheet1.png",
+		"images/player-sheet2.png",
+		"images/tiledbackground2.png",
+		"images/bullet-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
